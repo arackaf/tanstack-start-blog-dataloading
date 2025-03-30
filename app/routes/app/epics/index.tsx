41d290bb31535d3ee2dab3a7ca0c6748d709588b
Adam Rackis <adamrackis@hotmail.com>
2025-03-30 00:02:42 -0500

    work

--- app/routes/app/epics/index.tsx
+++ app/routes/app/epics/index.tsx
@@ -1,4 +1,4 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { epicsCountQueryOptions, epicsQueryOptions } from "../../../queries/epicsQuery";
 import { Fragment } from "react/jsx-runtime";
@@ -38,18 +38,18 @@ function Index() {
   const deferredPage = useDeferredValue(page);
   const loading = page !== deferredPage;
 
-  const { data: epicsData } = useSuspenseQuery(epicsQueryOptions(context.timestarted, deferredPage));
-  const { data: epicsCount } = useSuspenseQuery(epicsCountQueryOptions(context.timestarted));
+  const { data: epicsData } = useQuery(epicsQueryOptions(context.timestarted, deferredPage));
+  const { data: epicsCount } = useQuery(epicsCountQueryOptions(context.timestarted));
 
   return (
     <div className="flex flex-col gap-2 p-3">
       <h3 className="text-2xl">Epics page!</h3>
-      <h3 className="text-lg">There are {epicsCount.count} epics</h3>
+      <h3 className="text-lg">There are {epicsCount?.count} epics</h3>
       <div>
         <div
           className={`inline-grid gap-x-8 gap-y-4 grid-cols-[auto_auto_auto] items-center p-3 ${loading ? "opacity-40" : ""}`}
         >
-          {epicsData.map((e, idx) => (
+          {epicsData?.map((e, idx) => (
             <Fragment key={idx}>
               <div>{e.name}</div>
               <Link to="/app/epics/$epicId" params={{ epicId: e.id }} className="border p-1 rounded">
